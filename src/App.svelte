07806d458d52f328f4bf77183f70eea39205d72a
tmvc03 <no-replay@gmail.com>
2022-06-23 18:13:39 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<div class="telegram">
+  <div class="header">1</div>
+  <div class="body">2</div>
+  <div class="footer">3</div>
+</div>
+
+<style>
+  .telegram {
+    width: 375px;
+    height: 812px;
+    border: 1px solid red;
+  }
+</style>
